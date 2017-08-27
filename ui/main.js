732b1ee78706379c7abcf4ec2650c584d4b41627
alfvj17 <30633395+alfvj17@users.noreply.github.com>
2017-08-27 19:03:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
-var element = document.getElementById("main-text");
-element.innerHTML= "BE CHILL :)";
+
+var image = document.getElementById('madi');
+var marginleft = 0;
+function moveright() {
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveright,100);
+};
